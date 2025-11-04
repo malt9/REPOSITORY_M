c80a976d61a5528f04ed6870a5f7f734cfaefbbe
M. LopezTapia <187345511+malt9@users.noreply.github.com>
2025-11-04 13:08:01 -0300

    Delete TEST_CODES directory

--- TEST_CODES/CHANNEL_MAPS/ContourntoChannelMaps.py
+++
@@ -1,8 +0,0 @@
-from astropy.io import fits
-from astropy.wcs import WCS
-import matplotlib.pyplot as plt
-import matplotlib.patches as mpatches
-import numpy as np
-
-# Abrir el archivo FITS
-with fits.open('N159_CII_map.fit')
